2c2f984c48adc43fe6302a47ff76327b460bb0a9
ponbac <root@ponbac.xyz>
2024-12-01 22:32:50 +0100

    nicer

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::collections::HashMap;
 
+const INPUT: &str = include_str!("./input.txt");
+
 fn main() {
-    let input = include_str!("./input.txt").trim();
-    let lines = input.lines();
+    let lines = INPUT.lines();
 
     let (mut left_numbers, mut right_numbers): (Vec<i32>, Vec<i32>) = lines
         .map(|line| {
@@ -13,21 +14,20 @@ fn main() {
     left_numbers.sort();
     right_numbers.sort();
 
-    let total_distance = left_numbers
-        .iter()
-        .zip(right_numbers.iter())
-        .map(|(left, right)| (left - right).abs())
-        .sum::<i32>();
-    println!("Total distance: {}", total_distance);
-
     let right_counts = right_numbers.iter().fold(HashMap::new(), |mut acc, num| {
         *acc.entry(num).or_insert(0) += 1;
         acc
     });
 
-    let total_similarity = left_numbers
-        .iter()
-        .map(|left| left * right_counts.get(left).unwrap_or(&0))
-        .sum::<i32>();
+    let (total_distance, total_similarity) = left_numbers.iter().zip(right_numbers.iter()).fold(
+        (0, 0),
+        |(dist_acc, sim_acc), (left, right)| {
+            let distance = (left - right).abs();
+            let similarity = left * right_counts.get(left).unwrap_or(&0);
+            (dist_acc + distance, sim_acc + similarity)
+        },
+    );
+
+    println!("Total distance: {}", total_distance);
     println!("Total similarity: {}", total_similarity);
 }
