93788ba3586f0c4050dd18ce4159c5000d759838
ponbac <root@ponbac.xyz>
2024-12-04 08:05:34 +0100

    part 2

--- day-04/src/main.rs
+++ day-04/src/main.rs
@@ -19,10 +19,13 @@ struct Grid {
     rows: Vec<Vec<char>>,
     width: usize,
     height: usize,
+    search_pattern: Vec<char>,
+    search_pattern_reverse: Vec<char>,
+    search_length: usize,
 }
 
 impl Grid {
-    fn new(input: &str) -> Self {
+    fn new(input: &str, search_pattern: Vec<char>) -> Self {
         let rows: Vec<Vec<char>> = input
             .trim()
             .lines()
@@ -35,6 +38,9 @@ impl Grid {
             rows,
             width,
             height,
+            search_pattern: search_pattern.clone(),
+            search_pattern_reverse: search_pattern.iter().rev().cloned().collect(),
+            search_length: search_pattern.len(),
         }
     }
 
@@ -43,14 +49,16 @@ impl Grid {
     }
 
     fn find_horizontal(&self, x: usize, y: usize) -> (bool, Vec<(usize, usize)>) {
-        if x + 3 >= self.width {
+        if x + self.search_length > self.width {
             return (false, vec![]);
         }
 
-        let chars: Vec<char> = (0..4).map(|i| self.rows[y][x + i]).collect();
-        let coords: Vec<(usize, usize)> = (0..4).map(|i| (x + i, y)).collect();
+        let chars: Vec<char> = (0..self.search_length)
+            .map(|i| self.rows[y][x + i])
+            .collect();
+        let coords: Vec<(usize, usize)> = (0..self.search_length).map(|i| (x + i, y)).collect();
 
-        if chars == ['X', 'M', 'A', 'S'] || chars == ['S', 'A', 'M', 'X'] {
+        if chars == self.search_pattern || chars == self.search_pattern_reverse {
             (true, coords)
         } else {
             (false, vec![])
@@ -58,14 +66,16 @@ impl Grid {
     }
 
     fn find_vertical(&self, x: usize, y: usize) -> (bool, Vec<(usize, usize)>) {
-        if y + 3 >= self.height {
+        if y + self.search_length > self.height {
             return (false, vec![]);
         }
 
-        let chars: Vec<char> = (0..4).map(|i| self.rows[y + i][x]).collect();
-        let coords: Vec<(usize, usize)> = (0..4).map(|i| (x, y + i)).collect();
+        let chars: Vec<char> = (0..self.search_length)
+            .map(|i| self.rows[y + i][x])
+            .collect();
+        let coords: Vec<(usize, usize)> = (0..self.search_length).map(|i| (x, y + i)).collect();
 
-        if chars == ['X', 'M', 'A', 'S'] || chars == ['S', 'A', 'M', 'X'] {
+        if chars == self.search_pattern || chars == self.search_pattern_reverse {
             (true, coords)
         } else {
             (false, vec![])
@@ -77,11 +87,14 @@ impl Grid {
         x: usize,
         y: usize,
     ) -> (bool, Vec<(usize, usize)>) {
-        if x + 3 < self.width && y + 3 < self.height {
-            let chars: Vec<char> = (0..4).map(|i| self.rows[y + i][x + i]).collect();
-            let coords: Vec<(usize, usize)> = (0..4).map(|i| (x + i, y + i)).collect();
+        if x + self.search_length - 1 < self.width && y + self.search_length - 1 < self.height {
+            let chars: Vec<char> = (0..self.search_length)
+                .map(|i| self.rows[y + i][x + i])
+                .collect();
+            let coords: Vec<(usize, usize)> =
+                (0..self.search_length).map(|i| (x + i, y + i)).collect();
 
-            if chars == ['X', 'M', 'A', 'S'] || chars == ['S', 'A', 'M', 'X'] {
+            if chars == self.search_pattern || chars == self.search_pattern_reverse {
                 return (true, coords);
             }
         }
@@ -93,11 +106,14 @@ impl Grid {
         x: usize,
         y: usize,
     ) -> (bool, Vec<(usize, usize)>) {
-        if x + 3 < self.width && y >= 3 {
-            let chars: Vec<char> = (0..4).map(|i| self.rows[y - i][x + i]).collect();
-            let coords: Vec<(usize, usize)> = (0..4).map(|i| (x + i, y - i)).collect();
+        if x + self.search_length - 1 < self.width && y >= self.search_length - 1 {
+            let chars: Vec<char> = (0..self.search_length)
+                .map(|i| self.rows[y - i][x + i])
+                .collect();
+            let coords: Vec<(usize, usize)> =
+                (0..self.search_length).map(|i| (x + i, y - i)).collect();
 
-            if chars == ['X', 'M', 'A', 'S'] || chars == ['S', 'A', 'M', 'X'] {
+            if chars == self.search_pattern || chars == self.search_pattern_reverse {
                 return (true, coords);
             }
         }
@@ -126,19 +142,42 @@ impl Grid {
                 .collect(),
         )
     }
+
+    fn find_cross(&self, x: usize, y: usize) -> (bool, Vec<(usize, usize)>) {
+        if x + 2 >= self.width || y + 2 >= self.height {
+            return (false, vec![]);
+        }
+
+        let diagonal_down = self.find_diagonal_top_left_bottom_right(x, y);
+        println!("{:?}", diagonal_down);
+        let diagonal_up = self.find_diagonal_bottom_left_top_right(x, y + 2);
+        println!("{:?}", diagonal_up);
+
+        let count = diagonal_down.0 as usize + diagonal_up.0 as usize;
+        (
+            count == 2,
+            diagonal_down.1.into_iter().chain(diagonal_up.1).collect(),
+        )
+    }
 }
 
 fn main() {
-    let grid = Grid::new(INPUT);
-    println!("{}", grid);
+    let grid = Grid::new(INPUT, vec!['M', 'A', 'S']);
+    // println!("{}", grid);
 
     let mut found_grid: Vec<Vec<bool>> = vec![vec![false; grid.width]; grid.height];
 
     let mut found = 0;
+    let mut found_cross = 0;
     for y in 0..grid.height {
         for x in 0..grid.width {
-            let (count, coords) = grid.find_all(x, y);
-            found += count;
+            // let (count, coords) = grid.find_all(x, y);
+            // found += count;
+            // for (cx, cy) in coords {
+            //     found_grid[cy][cx] = true;
+            // }
+            let (cross, coords) = grid.find_cross(x, y);
+            found_cross += cross as usize;
             for (cx, cy) in coords {
                 found_grid[cy][cx] = true;
             }
@@ -160,6 +199,7 @@ fn main() {
     );
 
     println!("{}", found);
+    println!("{}", found_cross);
 }
 
 impl fmt::Display for Grid {
