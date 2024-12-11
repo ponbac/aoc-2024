37a89cb4289a94bf11a7bc5c25ffd89be7965369
ponbac <root@ponbac.xyz>
2024-12-11 21:01:53 +0100

    works maybe

--- day-11/src/main.rs
+++ day-11/src/main.rs
@@ -1,47 +1,68 @@
-use std::{fmt::Display, num::ParseIntError, str::FromStr};
+use std::collections::HashMap;
+use std::{fmt::Display, num::ParseIntError, str::FromStr, time::Instant};
 
 const INPUT: &str = include_str!("../input1.txt");
 
-struct Stone(u64);
+const EXAMPLE: &str = "125 17";
 
-impl Stone {
-    fn split(&self) -> Vec<Self> {
-        match self.0 {
-            0 => vec![Self(1)],
+fn blink(stones: &HashMap<u64, u64>) -> HashMap<u64, u64> {
+    let mut new_stones = HashMap::new();
+    for (&value, &count) in stones.iter() {
+        match value {
+            0 => {
+                *new_stones.entry(1).or_insert(0) += count;
+            }
             n if n.to_string().len() % 2 == 0 => {
                 let s = n.to_string();
                 let (left, right) = s.split_at(s.len() / 2);
-                vec![Self(left.parse().unwrap()), Self(right.parse().unwrap())]
+                let left_val = left.parse::<u64>().unwrap();
+                let right_val = right.parse::<u64>().unwrap();
+                new_stones
+                    .entry(left_val)
+                    .and_modify(|c| *c += count)
+                    .or_insert(count);
+                new_stones
+                    .entry(right_val)
+                    .and_modify(|c| *c += count)
+                    .or_insert(count);
             }
-            _ => vec![Self(self.0 * 2024)],
+            _ => {
+                *new_stones.entry(value * 2024).or_insert(0) += count;
+            }
         }
     }
-}
-
-fn blink(stones: &[Stone]) -> Vec<Stone> {
-    stones.iter().flat_map(|s| s.split()).collect()
+    new_stones
 }
 
 fn main() {
-    let stones = INPUT
+    let start = Instant::now();
+
+    let stones_input = INPUT
         .split_whitespace()
-        .map(|s| s.parse::<Stone>().unwrap())
+        .map(|s| s.parse::<u64>().unwrap())
         .collect::<Vec<_>>();
 
-    let final_stones = (0..75).fold(stones, |stones, _| blink(&stones));
-    println!("Part 1: {}", final_stones.len());
-}
-
-impl FromStr for Stone {
-    type Err = ParseIntError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(Self(s.parse().unwrap()))
+    let mut stones_map: HashMap<u64, u64> = HashMap::new();
+    for stone in stones_input {
+        *stones_map.entry(stone).or_insert(0) += 1;
     }
+
+    let final_stones = (0..75).fold(stones_map, |stones, _| blink(&stones));
+    println!("Part 1: {}", final_stones.values().sum::<u64>());
+
+    println!("Time: {:?}", start.elapsed());
 }
 
-impl Display for Stone {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.0)
-    }
-}
+// impl FromStr for Stone {
+//     type Err = ParseIntError;
+
+//     fn from_str(s: &str) -> Result<Self, Self::Err> {
+//         Ok(Self(s.parse().unwrap()))
+//     }
+// }
+
+// impl Display for Stone {
+//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//         write!(f, "{}", self.0)
+//     }
+// }
